056b5d661537e7464a6c81feecad6657b860c561
raihanuddin561 <raihanuddin561@gmail.com>
2019-01-23 06:57:17 +0600

    spring boot practice

---
+++ src/main/java/com/spring/vNoticeBoard/VNoticeBoardApplication.java
@@ -0,0 +1,14 @@
+package com.spring.vNoticeBoard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VNoticeBoardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VNoticeBoardApplication.class, args);
+	}
+
+}
+
---
+++ src/test/java/com/spring/vNoticeBoard/VNoticeBoardApplicationTests.java
@@ -0,0 +1,17 @@
+package com.spring.vNoticeBoard;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class VNoticeBoardApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
